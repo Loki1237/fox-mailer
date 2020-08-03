716680a1c749c9aff07cc32b8ac7ffedad20526e
LoKi1237 <loki1237@yandex.ru>
2020-08-03 15:23:43 +0300

    Add authentification controller

---
+++ backend/src/controllers/AuthController.ts
@@ -0,0 +1,78 @@
+import { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import { getRepository } from 'typeorm';
+import { User } from '../entities/User';
+import { AuthError } from '../errors/AuthError';
+import { Controller, Post, Req, Res } from 'routing-controllers';
+
+@Controller()
+export class AuthController {
+
+    @Post('/login')
+    async login(@Req() req: Request, @Res() res: Response) {
+        if (!process.env.JWT_SECRET) {
+            console.log('JWT_SECRET is required');
+            return res.status(500);
+        }
+
+        const { userName, password } = req.body;
+        const userRepository = getRepository(User);
+
+        if (!userName) {
+            return res.status(400).send(new AuthError('userName is required'));
+        }
+        if (!password) {
+            return res.status(400).send(new AuthError('password is required'));
+        }
+
+        const user = await userRepository.findOne({ userName });
+        if (!user) {
+            return res.status(400).send(new AuthError('incorrect userName or password'));
+        }
+
+        const validPassword = await bcrypt.compare(password, user.passwordHash);
+        if (!validPassword) {
+            return res.status(400).send(new AuthError('incorrect userName or password'));
+        }
+
+        const token = jwt.sign({ id: user.id, userName }, process.env.JWT_SECRET);
+        return res.status(200).cookie('AUTH_TOKEN', token).send();
+    }
+
+    @Post('/logout')
+    async logout(@Req() req: Request, @Res() res: Response) {
+        return res.clearCookie('AUTH_TOKEN').send();
+    }
+
+    @Post('/signup')
+    async signup(@Req() req: Request, @Res() res: Response) {
+        const { userName, password, firstName, lastName } = req.body;
+        const userRepository = getRepository(User);
+
+        if (!userName) {
+            return res.status(400).send(new AuthError('userName is required'));
+        }
+
+        const user = await userRepository.findOne({ userName });
+        if (user) {
+            return res.status(400).send(new AuthError('userName already taken'));
+        }
+
+        if (!password) {
+            return res.status(400).send(new AuthError('password is required'));
+        }
+
+        const passwordHash = await bcrypt.hash(password, 12);
+
+        const newUser = new User();
+        newUser.userName = userName;
+        newUser.passwordHash = passwordHash;
+        newUser.firstName = firstName;
+        newUser.lastName = lastName;
+        await userRepository.save(newUser);
+
+        return res.status(200).send();
+    }
+
+}
